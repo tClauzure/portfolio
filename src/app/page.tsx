999dbc90481a3958e980e3ea7b274fc4240a8033
tClauzure <theo.clauzure@outlook.fr>
2026-04-07 16:44:47 +0200

    prettier add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
-    <main style={{ backgroundColor: "var(--color-background)" }} className="min-h-screen p-12">
-      <h1 className="text-(--color-text-accent) text-4xl">Titre H1</h1>
-      <p className="text-(--color-text-muted) mt-4">Texte secondaire</p>
-      <div className="glass mt-8 p-6 w-64">
-        Carte glassmorphism
-      </div>
+    <main
+      style={{ backgroundColor: 'var(--color-background)' }}
+      className='min-h-screen p-12'
+    >
+      <h1 className='text-4xl text-(--color-text-accent)'>Titre H1</h1>
+      <p className='mt-4 text-(--color-text-muted)'>Texte secondaire</p>
+      <div className='glass mt-8 w-64 p-6'>Carte glassmorphism</div>
     </main>
   );
 }
