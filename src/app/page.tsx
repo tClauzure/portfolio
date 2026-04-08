12ff97154773ed9dac2612f6e4c232eba4f5184b
tClauzure <theo.clauzure@outlook.fr>
2026-04-08 17:28:46 +0200

    navbar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero';
+import Navbar from '@/components/Navbar';
 import ProjectGrid from '@/components/ProjectGrid';
 
 export default function Home() {
@@ -7,8 +8,11 @@ export default function Home() {
       style={{ backgroundColor: 'var(--background)' }}
       className='relative min-h-screen'
     >
+      <Navbar />
       <Hero />
       <ProjectGrid />
+      {/* Section contact — vide pour l'instant */}
+      <section id='contact' className='min-h-screen' />
     </main>
   );
 }
